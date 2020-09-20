c22810dde76639ece301365a538710909b00cc34
Ryan O'Shea <ryan@ryanoshea.com>
2020-09-20 17:30:07 -0400

    Disallow empty or null subDir

--- app/Models/DownloadConfig.ts
+++ app/Models/DownloadConfig.ts
@@ -2,10 +2,14 @@ import path from 'path';
 import { FILENAME_TEMPLATE } from '../Consts';
 
 export default class DownloadConfig {
-  public constructor(public outputDir: string, public subDir: string) {}
+  public constructor(public outputDir: string, public subDir: string | null) {
+    if (this.subDir === '' || this.subDir === 'null') {
+      this.subDir = null;
+    }
+  }
 
   public getOutDir(): string {
-    const pathParts = [this.outputDir, this.subDir, FILENAME_TEMPLATE].filter(part => part != null);
+    const pathParts = [this.outputDir, this.subDir, FILENAME_TEMPLATE].filter(part => part != null) as string[];
     return path.join(...pathParts);
   }
 }
